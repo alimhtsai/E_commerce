ba12c351f02233a689f0707b823e43c295c217c3
alimhtsai <48788292+alimhtsai@users.noreply.github.com>
2024-01-09 21:46:29 -0800

    Angular basics: nav bar, product interface, pagination interface

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Product } from './models/product';
+import { Pagination } from './models/pagination';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Skinet';
+  products: Product[] = [];
+
+  constructor(private http: HttpClient) {}
+  
+  ngOnInit(): void {
+    this.http.get<Pagination<Product[]>>('https://localhost:5001/api/products?pageSize=50').subscribe({
+      next: response => this.products = response.data, // what to do next
+      error: error => console.log(error), // what to do if there is an error
+      complete: () => {
+        console.log('request completed');
+        console.log('extra statment');
+      }
+    })
+  }
 }
