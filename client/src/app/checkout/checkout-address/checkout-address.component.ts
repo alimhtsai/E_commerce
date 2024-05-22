fc328f8591a1f930a459e6669b7f30d8cf9cc89e
alimhtsai <48788292+alimhtsai@users.noreply.github.com>
2024-05-22 15:03:20 -0700

    let user save as default address

--- client/src/app/checkout/checkout-address/checkout-address.component.ts
+++ client/src/app/checkout/checkout-address/checkout-address.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { FormGroup } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
+import { AccountService } from 'src/app/account/account.service';
 
 @Component({
   selector: 'app-checkout-address',
@@ -8,4 +10,15 @@ import { FormGroup } from '@angular/forms';
 })
 export class CheckoutAddressComponent {
   @Input() checkoutForm?: FormGroup;
+
+  constructor(private accountService: AccountService, private toastr: ToastrService) {}
+
+  saveUserAddress() {
+    this.accountService.updateUserAddress(this.checkoutForm?.get('addressForm')?.value).subscribe({
+      next: () => {
+        this.toastr.success('Address saved');
+        this.checkoutForm?.get('addressForm')?.reset(this.checkoutForm?.get('addressForm')?.value);
+      }
+    })
+  }
 }
