2fb440ff11e13a27e8f0e2500a1cb323b963cb1a
alimhtsai <48788292+alimhtsai@users.noreply.github.com>
2024-05-22 10:58:24 -0700

    create the delivery form and add delivery methods

--- client/src/app/checkout/checkout-delivery/checkout-delivery.component.ts
+++ client/src/app/checkout/checkout-delivery/checkout-delivery.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { DeliveryMethod } from 'src/app/shared/models/deliveryMethod';
+import { CheckoutService } from '../checkout.service';
 
 @Component({
   selector: 'app-checkout-delivery',
   templateUrl: './checkout-delivery.component.html',
   styleUrls: ['./checkout-delivery.component.scss']
 })
-export class CheckoutDeliveryComponent {
+export class CheckoutDeliveryComponent implements OnInit {
+  @Input() checkoutForm?: FormGroup;
+  deliveryMethods: DeliveryMethod[] = [];
 
+  constructor(private checkoutService: CheckoutService) {}
+
+  ngOnInit(): void {
+    this.checkoutService.getDeliveryMethods().subscribe({
+      next: dm => this.deliveryMethods = dm
+    })
+  }
 }
--- client/src/app/checkout/checkout.service.ts
+++ client/src/app/checkout/checkout.service.ts
@@ -1,9 +1,22 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { DeliveryMethod } from '../shared/models/deliveryMethod';
+import { map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CheckoutService {
+  baseUrl = environment.apiUrl;
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getDeliveryMethods() {
+    return this.http.get<DeliveryMethod[]>(this.baseUrl + 'orders/deliveryMethods').pipe(
+      map(dm => {
+        return dm.sort((a, b) => b.price - a.price)
+      })
+    )
+  }
 }
