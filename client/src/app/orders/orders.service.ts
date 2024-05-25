f507fb9f61cac6c9e330e446879d146874d452df
alimhtsai <48788292+alimhtsai@users.noreply.github.com>
2024-05-25 13:59:26 -0700

    3. add 2 methods in the order service to get the list of orders and a single order

--- client/src/app/orders/orders.service.ts
+++ client/src/app/orders/orders.service.ts
@@ -1,9 +1,23 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { Order } from '../shared/models/order';
 
 @Injectable({
   providedIn: 'root'
 })
 export class OrdersService {
 
-  constructor() { }
+  // 3. add 2 methods in the order service to get the list of orders and a single order
+
+  baseUrl = environment.apiUrl;
+
+  constructor(private http: HttpClient) { }
+
+  getOrdersForUser() {
+    return this.http.get<Order[]>(this.baseUrl + 'orders');
+  }
+  getOrderDetailed(id: number) {
+    return this.http.get<Order>(this.baseUrl + 'orders/' + id);
+  }
 }
