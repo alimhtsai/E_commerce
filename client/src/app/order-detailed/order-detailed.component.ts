a4f789f63a5b63f1f62bd05d66d682692bfc4be8
alimhtsai <48788292+alimhtsai@users.noreply.github.com>
2024-05-25 14:11:59 -0700

    6. in the order-detailed component get the individual order and display it in the browser

--- client/src/app/order-detailed/order-detailed.component.ts
+++ client/src/app/order-detailed/order-detailed.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { BreadcrumbService } from 'xng-breadcrumb';
+import { OrdersService } from '../orders/orders.service';
+import { Order } from '../shared/models/order';
 
 @Component({
   selector: 'app-order-detailed',
   templateUrl: './order-detailed.component.html',
   styleUrls: ['./order-detailed.component.scss']
 })
-export class OrderDetailedComponent {
+export class OrderDetailedComponent implements OnInit {
+
+  // 6. in the order-detailed component get the individual order and display it in the browser
+  order?: Order;
+
+  constructor(private orderService: OrdersService, private route: ActivatedRoute, private bcService: BreadcrumbService) {
+      this.bcService.set('@OrderDetailed', ' ');
+  }
+
+  ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    id && this.orderService.getOrderDetailed(+id).subscribe({
+      next: order => {
+        this.order = order;
+        this.bcService.set('@OrderDetailed', `Order# ${order.id} - ${order.status}`);
+      }
+    })
+  }
 
 }
