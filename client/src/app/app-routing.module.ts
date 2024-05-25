4c2c9e6fdcbaa13656a4c856388b2f2c7ab83208
alimhtsai <48788292+alimhtsai@users.noreply.github.com>
2024-05-25 13:56:58 -0700

    2. lazy load the orders module in the app-routing.module.ts

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -7,19 +7,52 @@ import { ServerErrorComponent } from './core/server-error/server-error.component
 import { AuthGuard } from './core/guards/auth.guard';
 
 const routes: Routes = [
-  {path: '', component: HomeComponent, data: {breadcrumb: 'Home'}},
-  {path: 'test-error', component: TestErrorComponent},
-  {path: 'not-found', component: NotFoundComponent},
-  {path: 'server-error', component: ServerErrorComponent},
-  {path: 'shop', loadChildren: () => import('./shop/shop.module').then(m => m.ShopModule)},
-  {path: 'basket', loadChildren: () => import('./basket/basket.module').then(m => m.BasketModule)},
+  {
+    path: '', 
+    component: HomeComponent, 
+    data: {breadcrumb: 'Home'}
+  },
+  {
+    path: 'test-error', 
+    component: TestErrorComponent
+  },
+  {
+    path: 'not-found', 
+    component: NotFoundComponent
+  },
+  {
+    path: 'server-error', 
+    component: ServerErrorComponent
+  },
+  {
+    path: 'shop', 
+    loadChildren: () => import('./shop/shop.module').then(m => m.ShopModule)
+  },
+  {
+    path: 'basket', 
+    loadChildren: () => import('./basket/basket.module').then(m => m.BasketModule)
+  },
   {
     path: 'checkout', 
     canActivate: [AuthGuard],
     loadChildren: () => import('./checkout/checkout.module').then(m => m.CheckoutModule)
   },
-  {path: 'account', loadChildren: () => import('./account/account.module').then(m => m.AccountModule)},
-  {path: '**', redirectTo: '', pathMatch: 'full'}
+
+  // 2. lazy load the orders module in the app-routing.module.ts
+  {
+    path: 'orders', 
+    canActivate: [AuthGuard],
+    loadChildren: () => import('./orders/orders.module').then(m => m.OrdersModule)
+  },
+  {
+    path: 'account', 
+    loadChildren: () => import('./account/account.module').then(m => m.AccountModule)
+  },
+  {
+    path: '**', 
+    redirectTo: '', 
+    pathMatch: 'full'
+  }
 ];
 
 @NgModule({
