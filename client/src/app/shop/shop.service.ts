907ce7ee3dada645ce4d87e13ebc98f1697e28de
alimhtsai <48788292+alimhtsai@users.noreply.github.com>
2024-05-25 16:59:14 -0700

    cache on the client side by caching in the service

--- client/src/app/shop/shop.service.ts
+++ client/src/app/shop/shop.service.ts
@@ -5,12 +5,16 @@ import { Product } from '../shared/models/product';
 import { Brand } from '../shared/models/brand';
 import { Type } from '../shared/models/type';
 import { ShopParams } from '../shared/models/shopParams';
+import { map, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ShopService {
   baseUrl = 'https://localhost:5001/api/';
+  products: Product[] = [];
+  brands: Brand[] = [];
+  types: Type[] = [];
 
   constructor(private http: HttpClient) { }
 
@@ -25,18 +29,35 @@ export class ShopService {
 
     if (shopParams.search) params = params.append('search', shopParams.search);
 
-    return this.http.get<Pagination<Product[]>>(this.baseUrl + 'products', {params});
+    return this.http.get<Pagination<Product[]>>(this.baseUrl + 'products', {params}).pipe(
+      map(response => {
+        this.products = response.data;
+        return response;
+      })
+    )
   }
 
   getProduct(id: number) {
+    const product = this.products.find(p => p.id === id);
+
+    if (product) return of(product);
+
     return this.http.get<Product>(this.baseUrl + 'products/' + id);
   }
   
   getBrands() {
-    return this.http.get<Brand[]>(this.baseUrl + 'products/brands');
+    if (this.brands.length > 0) return of(this.brands);
+
+    return this.http.get<Brand[]>(this.baseUrl + 'products/brands').pipe(
+      map(brands => this.brands = brands)
+    );
   }
 
   getTypes() {
-    return this.http.get<Type[]>(this.baseUrl + 'products/types');
+    if (this.types.length > 0) return of(this.types);
+
+    return this.http.get<Type[]>(this.baseUrl + 'products/types').pipe(
+      map(types => this.types = types)
+    );
   }
 }
