ac09eda4ba1bb82f3c82475f7380ecdc6cbdf38e
alimhtsai <48788292+alimhtsai@users.noreply.github.com>
2024-05-25 16:41:14 -0700

    add cached() to the products controller

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -29,6 +29,7 @@ namespace API.controller
             _productBrandRepo = productBrandRepo;
         }
 
+        [Cached(600)]
         [HttpGet]
         public async Task<ActionResult<Pagination<ProductToReturnDto>>> GetProducts(
             [FromQuery]ProductSpecParams productParams)
@@ -42,6 +43,7 @@ namespace API.controller
                 productParams.PageSize, totalItems, data));
         }
 
+        [Cached(600)]
         [HttpGet("{id}")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status404NotFound)]
@@ -53,12 +55,14 @@ namespace API.controller
             return _mapper.Map<Product, ProductToReturnDto>(product);
         }
 
+        [Cached(600)]
         [HttpGet("brands")]
         public async Task<ActionResult<IReadOnlyList<ProductBrand>>> GetProductBrands()
         {
             return Ok(await _productBrandRepo.ListAllAsync());
         }
 
+        [Cached(600)]
         [HttpGet("types")]
         public async Task<ActionResult<IReadOnlyList<ProductBrand>>> GetProductTypes()
         {
