c53ea100979b919e97f34e9bf3f692b617fae1fa
alimhtsai <48788292+alimhtsai@users.noreply.github.com>
2024-05-25 14:23:42 -0700

    7. update the checkout-success page to allow the user to view the created order or go to the orders list

--- client/src/app/checkout/checkout-success/checkout-success.component.ts
+++ client/src/app/checkout/checkout-success/checkout-success.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { Order } from 'src/app/shared/models/order';
 
 @Component({
   selector: 'app-checkout-success',
@@ -7,4 +9,11 @@ import { Component } from '@angular/core';
 })
 export class CheckoutSuccessComponent {
 
+  // 7. update the checkout-success page to allow the user to view the created order or go to the orders list
+  order?: Order;
+
+  constructor(private router: Router) {
+    const navigation = this.router.getCurrentNavigation();
+    this.order = navigation?.extras?.state as Order
+  }
 }
