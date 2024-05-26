257a6c423043322bcfa2b32a7fdb2ab09b487992
alimhtsai <48788292+alimhtsai@users.noreply.github.com>
2024-05-26 16:21:07 -0700

    adjust styling

--- client/src/app/core/interceptor/loading.interceptor.ts
+++ client/src/app/core/interceptor/loading.interceptor.ts
@@ -16,7 +16,8 @@ export class LoadingInterceptor implements HttpInterceptor {
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
     if (
       request.url.includes('emailExists') ||
-      request.method === 'POST' && request.url.includes('orders')
+      request.method === 'POST' && request.url.includes('orders') ||
+      request.method === 'DELETE'
     ) {
       return next.handle(request);
     }
