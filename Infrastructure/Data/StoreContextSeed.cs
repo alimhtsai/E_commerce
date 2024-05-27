7eb63188b8a16cbe4dd405877d77fffe8be4e6a4
alimhtsai <48788292+alimhtsai@users.noreply.github.com>
2024-05-26 21:21:39 -0700

    pre deployment work

--- Infrastructure/Data/StoreContextSeed.cs
+++ Infrastructure/Data/StoreContextSeed.cs
@@ -1,3 +1,4 @@
+using System.Reflection;
 using System.Text.Json;
 using Core.Entities;
 using Core.Entities.OrderAggregate;
@@ -9,30 +10,32 @@ namespace Infrastructure.Data
     {
         public static async Task SeedAsync(StoreContext context)
         {
+            var path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+
             if (!context.ProductBrands.Any())
             {
-                var brandsData = File.ReadAllText("../Infrastructure/Data/SeedData/brands.json");
+                var brandsData = File.ReadAllText(path + @"/Data/SeedData/brands.json");
                 var brands = JsonSerializer.Deserialize<List<ProductBrand>>(brandsData);
                 context.ProductBrands.AddRange(brands);
             }
 
             if (!context.ProductTypes.Any())
             {
-                var typesData = File.ReadAllText("../Infrastructure/Data/SeedData/types.json");
+                var typesData = File.ReadAllText(path + @"/Data/SeedData/types.json");
                 var types = JsonSerializer.Deserialize<List<ProductType>>(typesData);
                 context.ProductTypes.AddRange(types);
             }
 
             if (!context.Products.Any())
             {
-                var productsData = File.ReadAllText("../Infrastructure/Data/SeedData/products.json");
+                var productsData = File.ReadAllText(path + @"/Data/SeedData/products.json");
                 var products = JsonSerializer.Deserialize<List<Product>>(productsData);
                 context.Products.AddRange(products);
             }
 
             if (!context.DeliveryMethods.Any())
             {
-                var deliveryData = File.ReadAllText("../Infrastructure/Data/SeedData/delivery.json");
+                var deliveryData = File.ReadAllText(path + @"/Data/SeedData/delivery.json");
                 var methods = JsonSerializer.Deserialize<List<DeliveryMethod>>(deliveryData);
                 context.DeliveryMethods.AddRange(methods);
             }
